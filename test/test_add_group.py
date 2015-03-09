feaa080edccbed5b928beab6189d57eaee7324a7
Allan <nurulla@gmail.com>
2015-03-09 00:24:56 -0400

    "Session" management class dedicated

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_groups(Group(name="Group1", header="Logo", footer="Comments"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_groups(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
